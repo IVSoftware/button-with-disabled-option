cb1f9357c57f7a27b9a634ee678822135f2c2c27
Tom Gregor <tgregor@ivsoftware.com>
2022-07-28 14:47:38 -0600

    Add project files.

---
+++ button-with-disabled-option/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace button_with_disabled_option
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // this.buttonWithDisableOption = new System.Windows.Forms.Button();
+            this.buttonWithDisableOption = new button_with_disabled_option.ButtonWithDisableOption();
+
+            this.checkBoxButtonEnabled = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonWithDisableOption
+            // 
+            this.buttonWithDisableOption.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonWithDisableOption.Enabled = false;
+            this.buttonWithDisableOption.Location = new System.Drawing.Point(27, 45);
+            this.buttonWithDisableOption.Name = "buttonWithDisableOption";
+            this.buttonWithDisableOption.Size = new System.Drawing.Size(250, 35);
+            this.buttonWithDisableOption.TabIndex = 0;
+            this.buttonWithDisableOption.Text = "Button with Disable Option";
+            this.buttonWithDisableOption.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxButtonEnabled
+            // 
+            this.checkBoxButtonEnabled.AutoSize = true;
+            this.checkBoxButtonEnabled.Location = new System.Drawing.Point(27, 146);
+            this.checkBoxButtonEnabled.Name = "checkBoxButtonEnabled";
+            this.checkBoxButtonEnabled.Size = new System.Drawing.Size(159, 29);
+            this.checkBoxButtonEnabled.TabIndex = 1;
+            this.checkBoxButtonEnabled.Text = "Button Enabled";
+            this.checkBoxButtonEnabled.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.checkBoxButtonEnabled);
+            this.Controls.Add(this.buttonWithDisableOption);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // private System.Windows.Forms.Button button1;
+        private button_with_disabled_option.ButtonWithDisableOption buttonWithDisableOption;
+        private System.Windows.Forms.CheckBox checkBoxButtonEnabled;
+    }
+}
+
---
+++ button-with-disabled-option/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button_with_disabled_option
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonWithDisableOption.Enabled = checkBoxButtonEnabled.Checked = true;
+
+            // Button events
+            buttonWithDisableOption.Click += buttonWithDisableOption_Click;
+            buttonWithDisableOption.DisabledClick += buttonWithDisableOption_DisabledClick;
+
+            // CheckBox events
+            checkBoxButtonEnabled.CheckedChanged += checkBoxButtonEnabled_CheckedChanged;
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+        }
+        private void checkBoxButtonEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            buttonWithDisableOption.Enabled = checkBoxButtonEnabled.Checked;
+        }
+        private void buttonWithDisableOption_Click(object sender, EventArgs e)
+        {
+            // Title bar
+            Text = $"Button Click {_tstcount++} (Enabled)";  
+        }
+        private void buttonWithDisableOption_DisabledClick(object sender, EventArgs e)
+        {
+            var enabled = buttonWithDisableOption.Enabled ? "Enabled" : "Disabled";
+            // Title bar
+            Text = $"Button Click {_tstcount++} (Disabled)";
+            MessageBox.Show("Button is unclickable");
+        }
+        int _tstcount = 1;
+    }
+    class ButtonWithDisableOption : Button
+    {
+        bool _enabled = false;
+        public new bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                if (!Equals(_enabled, value))
+                {
+                    _enabled = value;
+                    OnEnabledChanged(EventArgs.Empty);
+                }
+            }
+        }
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            base.OnEnabledChanged(e);
+            if(Enabled)
+            {
+                ForeColor = SystemColors.ControlText;
+                BackColor = SystemColors.Control;
+            }
+            else
+            {
+                ForeColor = Color.FromArgb(191, 191, 191);
+                BackColor = Color.FromArgb(204, 204, 204);
+            }
+        }
+        protected override void OnClick(EventArgs e)
+        {
+            if (Enabled)
+            {
+                base.OnClick(e);
+            }
+            else
+            {
+                DisabledClick?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public event EventHandler DisabledClick;
+    }
+}
